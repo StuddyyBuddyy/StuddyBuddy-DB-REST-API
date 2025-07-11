3137359dcb483d23f2c1f0d0d569cdc0308b7b8b
eliasra-aau <eliasra@edu.aau.at>
2025-07-11 11:38:55 +0200

    added docker stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,3 +38,7 @@ dependencies {
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
+java {
+    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
+}
