b4a16b2af1d73b7fb69f8c8560eda61a329caa87
eliasra-aau <eliasra@edu.aau.at>
2025-07-11 10:25:14 +0200

    initial commit with preconfigured project from ktor

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,13 @@
+package appdev.studdybuddy
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureMonitoring()
+    configureDatabases()
+    configureRouting()
+}
